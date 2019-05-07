737bd7d1f9adc739f6e4c24f988969d9c9cac46d
Dani O'Quinn <danieljoquinn@gmail.com>
2019-05-06 21:27:44 -0400

    allow cross origin gets for development

--- blog/joshi_blog/settings.py
+++ blog/joshi_blog/settings.py
@@ -31,7 +31,6 @@ ALLOWED_HOSTS = [
 ]
 print('ACCESS over SSH => http://raspberrypi/admin')
 
-
 # Application definition
 
 INSTALLED_APPS = [
@@ -49,7 +48,7 @@ MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
+#    'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
@@ -118,6 +117,8 @@ USE_I18N = True
 USE_L10N = True
 
 USE_TZ = True
+
+CORS_ORIGIN_ALLOW_ALL = True
 
 
 # Static files (CSS, JavaScript, Images)
