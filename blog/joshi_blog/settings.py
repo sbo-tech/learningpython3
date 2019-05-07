fa8f7856cdc10a02ce7b6ccabb2b3fe89da2fff7
Dani O'Quinn <danieljoquinn@gmail.com>
2019-05-06 22:03:34 -0400

    add cross origin package to requirements.txt

--- blog/joshi_blog/settings.py
+++ blog/joshi_blog/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'posts',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
@@ -49,10 +50,17 @@ MIDDLEWARE = [
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
 #    'django.middleware.csrf.CsrfViewMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.common.CommonMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+
+
+CORS_ORIGIN_WHITELIST = (
+    'localhost:3000',
+)
 
 ROOT_URLCONF = 'joshi_blog.urls'
 
